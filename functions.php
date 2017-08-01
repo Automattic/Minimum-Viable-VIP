2f185202522456a435e2dbc7e2d3010892a91a7a
Tom J Nowell <contact@tomjn.com>
2017-08-01 15:39:20 +0100

    add hello dolly as an example plugin

--- functions.php
+++ functions.php
@@ -1,6 +1,22 @@
 <?php
 
+/**
+ * Load up the VIP code, without this VIP functions will fail,
+ * and it must be the first thing loaded in functions.php
+ */
 require_once( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' );
+
+/**
+ * Now lets load our plugins, these should be in a subfolder,
+ * in this theme that subfolder is "plugins", but it could be
+ * something else
+ *
+ * For .com themes, there is no access to the main wp-content/plugins
+ * or mu-plugins folder, so plugins you would like to use
+ * need to be bundled with the theme. You don't need to do this for
+ * Jetpack and other shared plugins already on .com
+ */
+require_once( dirname( __FILE__ ) . '/plugins/hello.php' );
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
